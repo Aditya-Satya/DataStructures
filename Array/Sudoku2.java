fd1576abe23208c42c5d1896609b83f44dae3896
Aditya <73811047+imAdityaSatya@users.noreply.github.com>
2022-03-26 22:07:09 +0530

    Update Sudoku2.java

--- Array/Sudoku2.java
+++ Array/Sudoku2.java
@@ -1,20 +1,20 @@
 import java.util.*;
 public class Sudoku2 {
 	public static boolean isValidSudoku(List<List<Integer>> partialAssignment){
-		for (int i=0;i<partialAssignment.size();++i) {
+		for(int i=0;i<partialAssignment.size();++i) {
 			if (hasDuplicate(partialAssignment,i,i+1,0,partialAssignment.size())){
 				return false;
 			}
 		}
-		for (int j=0;j<partialAssignment.size();++j) {
-			if (hasDuplicate(partialAssignment,0,partialAssignment.size(),j,j+1)) {
+		for(int j=0;j<partialAssignment.size();++j) {
+			if(hasDuplicate(partialAssignment,0,partialAssignment.size(),j,j+1)) {
 				return false;
 			}
 		}
 		int regionSize=(int)Math.sqrt(partialAssignment.size());
-		for (int I=0;I<regionSize;++I) {
-			for (int J=0;J<regionSize;++J) {
-				if (hasDuplicate(partialAssignment,regionSize*I,regionSize*(I+1),regionSize*J,regionSize*(J+1))){
+		for(int I=0; I<regionSize; ++I) {
+			for (int J=0; J<regionSize; ++J) {
+				if(hasDuplicate(partialAssignment,regionSize*I,regionSize*(I+1),regionSize*J,regionSize*(J+1))){
 					return false;
 				}
 			}
@@ -23,9 +23,9 @@ public class Sudoku2 {
 	}
 	private static boolean hasDuplicate (List <List<Integer>> partialAssignment,int startRow,int endRow,int startCol,int endCol ) {
 		List<Boolean> isPresent=new ArrayList<>(Collections.nCopies (partialAssignment.size()+1,false));
-		for (int i=startRow;i<endRow; ++i) {
-			for (int j=startCol;j<endCol;++j) {
-				if (partialAssignment.get(i).get(j)!=0&&isPresent.get(partialAssignment.get(i).get (j))) {
+		for(int i=startRow;i<endRow; ++i) {
+			for(int j=startCol;j<endCol;++j) {
+				if(partialAssignment.get(i).get(j)!=0&&isPresent.get(partialAssignment.get(i).get (j))) {
 					return true ;
 				}
 				isPresent.set(partialAssignment.get(i).get(j), true);
@@ -52,10 +52,10 @@ public class Sudoku2 {
 		}
 		boolean result = isValidSudoku(l);
 		if(result){
-			System.out.println("Valid.");
+			System.out.println("Valid");
 		}
 		else{
-			System.out.println("Invalid.");
+			System.out.println("Invalid");
 		}	
 	}
 }
