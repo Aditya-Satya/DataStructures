d8eca596ab1c5f3bea8bb86cb334143dabe23b20
Aditya <73811047+imAdityaSatya@users.noreply.github.com>
2022-03-26 22:11:19 +0530

    Update Sudoku.java

--- Array/Sudoku.java
+++ Array/Sudoku.java
@@ -2,17 +2,18 @@ import java.util.*;
 import java.util.stream.*;
 public class Sudoku {
 	public static boolean isValidSudoku(List<List<Integer>>partialAssignment) {
-		for(int i = 0; i < partialAssignment.size(); i++) {
-			if(hasDuplicate(partialAssignment, i, i+1, 0, partialAssignment.size())) {
+		int n= partialAssignment.size();
+		for(int i=0; i<n; i++) {
+			if(hasDuplicate(partialAssignment, i, i+1, 0, n)) {
 				return false;
 			}
 		}
 		for(int j=0; j<partialAssignment.size(); j++) {
-			if(hasDuplicate(partialAssignment, 0, partialAssignment.size(), j, j + 1)) {
+			if(hasDuplicate(partialAssignment, 0, n, j, j + 1)) {
 				return false;
 			}
 		}
-		int regionSize= (int)Math.sqrt(partialAssignment.size());
+		int regionSize= (int)Math.sqrt(n);
 		for(int I=0; I<regionSize; I++) {
 			for(int J=0; J<regionSize; J++) {
 				if(hasDuplicate(partialAssignment, regionSize*I, regionSize*(I+1), regionSize*J, regionSize*(J+1))) {
@@ -22,12 +23,12 @@ public class Sudoku {
 		}return true;
 	}
 	private static boolean hasDuplicate (List <List<Integer>>partialAssignment, int startRow, int endRow, int startCol, int endCol) {
-		List <Boolean> isPresent = new ArrayList<>(Collections . nCopies (partialAssignment . size () + 1, false));
-		for (int i = startRow; i < endRow; ++i) {
-			for (int j = startCol; j < endCol; ++j) {
-				if (partialAssignment . get (i) . get (j) != 0 && isPresent . get (partialAssignment . get (i) . get (j )) ) {
-					return true ;
-				}isPresent .set (partialAssignment .get(i).get(j) , true);
+		List <Boolean> isPresent= new ArrayList<>(Collections.nCopies(n+1, false));
+		for(int i=startRow; i<endRow; ++i) {
+			for(int j=startCol; j<endCol; ++j) {
+				if(partialAssignment.get(i).get(j)!=0 && isPresent.get(partialAssignment.get (i).get (j)) ) {
+					return true;
+				}isPresent.set(partialAssignment.get(i).get(j), true);
 			}
 		}return false;
 	}
